829148d232a741d63da34bccf051eb2cce212ab4
Butschster <butschster@gmail.com>
2015-03-28 01:31:00 +0300

    обновление сжатых файлов JS и CSS

--- Gulpfile.js
+++ Gulpfile.js
@@ -8,13 +8,6 @@ KodiCMS(function(mix) {
 		mix
 			.less('common.less')
 			.scripts([
-				'cms/media/libs/bootstrap-3.3.1/dist/js/bootstrap.js',
-				'cms/media/libs/pnotify/jquery.pnotify.min.js',
-				'cms/media/libs/select2/select2.min.js',
-				'cms/media/libs/dropzone/min/dropzone.min.js',
-				'cms/media/libs/fancybox/jquery.fancybox.pack.js',
-				'cms/media/libs/datepicker/jquery.datetimepicker.min.js',
-				'cms/media/libs/underscore-min.js',
 				'cms/media/js/core.js',
 				'cms/media/js/backend.js'
 			], null, './');
